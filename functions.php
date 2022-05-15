72e249f9cc78f06616c74dadd99a6d0892426f01
Srdjan Mitrovic <iam@mitrovics.com>
2022-05-16 00:04:43 +0200

    remove search

--- functions.php
+++ functions.php
@@ -171,3 +171,18 @@ add_filter('use_block_editor_for_post', '__return_false', 10);
 function yoasttobottom() {
 	return 'low';
 }
+
+function fb_filter_query( $query, $error = true ) {
+
+    if ( is_search() ) {
+        $query->is_search = false;
+        $query->query_vars['s'] = false;
+        $query->query['s'] = false;
+
+        if ( $error == true )
+            $query->is_404 = true;
+    }
+}
+
+add_action( 'parse_query', 'fb_filter_query' );
+add_filter( 'get_search_form', function() { return null;} );
